349326604bb5c59084874a22273278396a821ad8
Emrah <emrahkivanc@gmail.com>
2020-02-25 01:19:53 +0100

    Test File updated

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Orders list', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Orders List/i);
   expect(linkElement).toBeInTheDocument();
 });
